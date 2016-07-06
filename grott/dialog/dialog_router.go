561e130436992b84d636289b61307c39d2c80df5
Michael Golfi <michael.m.golfi@gmail.com>
2016-07-05 23:55:07 -0700

    Added go func and chans to HandleMessage

--- grott/dialog/dialog_router.go
+++ grott/dialog/dialog_router.go
@@ -26,18 +26,36 @@ func StartWithStorage(dialogs []types.Dialoger, storage storage.Storage) *Dialog
 	}
 }
 
-func (r *DialogRouter) HandleMessage(msg types.Message) error {
+func (router *DialogRouter) HandleMessage(message types.Message) (types.Message, error) {
 
-	highestIndex := getHighestScoringDialog(r.Dialogs, msg)
+	respChan := make(chan types.Message, 1)
+	errChan := make(chan error, 1)
 
-	msgCtx,err := r.ContextStorage.Get(msg.ConversationId)
-	if err != nil {
-		return err
-	}
+	go func(r chan types.Message, e chan error, d DialogRouter, m types.Message) {
+		highestIndex := 0
+		if len(d.Dialogs) > 1 {
+			highestIndex = getHighestScoringDialog(d.Dialogs, m)
+		}
 
-	r.Dialogs[highestIndex].MessageReceived(msgCtx, msg)
+		msgCtx, err := d.ContextStorage.Get(m.ConversationId)
+		if err != nil {
+			e <- err
+			r <- nil
+			return
+		}
 
-	return nil
+		resp, err := d.Dialogs[highestIndex].MessageReceived(msgCtx, m)
+		if err != nil {
+			e <- err
+			r <- nil
+			return
+		}
+
+		r <- resp
+		e <- nil
+	}(respChan, errChan, router, message)
+
+	return <-respChan, <-errChan
 }
 
 func getHighestScoringDialog(dialogs []types.Dialoger, msg types.Message) *types.Dialoger {
